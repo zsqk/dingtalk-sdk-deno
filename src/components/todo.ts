97d3aa16d07913d1f329dc3d088d07f3b8be43b4
iugo <iugogogo@gmail.com>
2022-08-24 10:09:30 +0800

    chore: 完善 addDingtalkTodo 参数及命名

--- src/components/todo.ts
+++ src/components/todo.ts
@@ -6,35 +6,54 @@
  *
  * @author Lian Zheren <lzr@go0356.com>
  */
-export async function addTask({ unionID, operatorID }: {
-  /** 要分配给谁 unionid */
-  unionID: string;
-  /** 谁分配的 unionid */
-  operatorID?: string;
-  /** 待办标题 */
-  subject: string;
-  /** 待办描述 */
-  description?: string;
-  /** 截止时间, UNIX 时间戳 毫秒 */
-  dueTime?: number;
-  /** 执行者 unionid */
-  executorIds?: string[];
-  /** 参与者 unionid */
-  participantIds?: string[];
-  /** 生成的待办是否仅展示在执行者的待办列表中 */
-  isOnlyShowExecutor?: boolean;
-  /** 优先级 */
-  priority?: 10 | 20 | 30 | 40;
-}, token: string) {
+export async function addDingtalkTodo(
+  {
+    unionID,
+    subject,
+    description,
+    dueTime,
+    executorUnionIDs,
+    participantUnionIDs,
+    isOnlyShowExecutor = false,
+    priority,
+  }: {
+    /** 要分配给谁 unionid */
+    unionID: string;
+    /** 待办标题 */
+    subject: string;
+    /** 待办描述 */
+    description?: string;
+    /** 截止时间, UNIX 时间戳 毫秒 */
+    dueTime?: number;
+    /** 执行者 unionid */
+    executorUnionIDs?: string[];
+    /** 参与者 unionid */
+    participantUnionIDs?: string[];
+    /** 生成的待办是否仅展示在执行者的待办列表中, 默认否 */
+    isOnlyShowExecutor?: boolean;
+    /** 优先级 */
+    priority?: 10 | 20 | 30 | 40;
+  },
+  token: string,
+) {
   const path = `https://api.dingtalk.com/v1.0/todo/users/${unionID}/tasks`;
-  const url = new URL(path);
-  operatorID && url.searchParams.set('operatorId', operatorID);
-  const res = await fetch(url, {
+  // 经实测, 文档中描述的 operatorId 参数并无额外作用.
+  // 如果 operatorId 与 unionID 不一致, 则无任何效果, 也不报错.
+  const res = await fetch(path, {
     method: 'POST',
     headers: {
       'x-acs-dingtalk-access-token': token,
       'Content-Type': 'application/json',
     },
+    body: JSON.stringify({
+      subject,
+      description,
+      dueTime,
+      executorIds: executorUnionIDs,
+      participantIds: participantUnionIDs,
+      isOnlyShowExecutor,
+      priority,
+    }),
   });
   if (res.status >= 500) {
     throw new Error(`DINGTALK HTTP ${res.status}`);
