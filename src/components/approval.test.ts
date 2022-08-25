eeb96aac599d995db3eeb79afc22755c7b8c54e2
iugo <iugogogo@gmail.com>
2022-08-25 15:59:20 +0800

    test: 完善 approval 测试

--- src/components/approval.test.ts
+++ src/components/approval.test.ts
@@ -1,14 +1,37 @@
-import { addDingtalkApprovalInstance } from './approval.ts';
+import {
+  addDingtalkApprovalInstance,
+  getDingtalkApprovalProcess,
+} from './approval.ts';
 
 const DINGTALK_ACCESSTOKEN = Deno.env.get('DINGTALK_ACCESSTOKEN') ?? '';
 const DINGTALK_USERID = Deno.env.get('DINGTALK_USERID') ?? '';
 const DINGTALK_PROCESSCODE = Deno.env.get('DINGTALK_PROCESSCODE') ?? '';
 
-Deno.test('addDingtalkApprovalInstance', async () => {
+Deno.test('addDingtalkApprovalInstance-base', async () => {
   const res = await addDingtalkApprovalInstance({
     originatorUserId: DINGTALK_USERID,
     processCode: DINGTALK_PROCESSCODE,
     formComponentValues: [{ name: '审批内容', value: new Date().toString() }],
   }, DINGTALK_ACCESSTOKEN);
+  console.log(res);
+});
+
+Deno.test('addDingtalkApprovalInstance-opt', async () => {
+  const res = await addDingtalkApprovalInstance({
+    originatorUserId: DINGTALK_USERID,
+    processCode: DINGTALK_PROCESSCODE,
+    formComponentValues: [
+      { name: '审批内容', value: new Date().toString() },
+      { name: '审批金额', value: '100' },
+    ],
+  }, DINGTALK_ACCESSTOKEN);
+  console.log(res);
+});
+
+Deno.test('getDingtalkApprovalProcess', async () => {
+  const res = await getDingtalkApprovalProcess(
+    DINGTALK_PROCESSCODE,
+    DINGTALK_ACCESSTOKEN,
+  );
   console.log(res);
 });
