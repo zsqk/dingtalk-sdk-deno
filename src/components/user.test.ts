e4d2a51483f32d762d5270fc3913611cd61af447
iugo <iugogogo@gmail.com>
2022-08-25 15:47:54 +0800

    test: getDingtalkUserIDByPhonenum

--- src/components/user.test.ts
+++ src/components/user.test.ts
@@ -1,8 +1,13 @@
-import { getDingtalkUserByDeptID, getDingtalkUserByUserID } from './user.ts';
+import {
+  getDingtalkUserByDeptID,
+  getDingtalkUserByUserID,
+  getDingtalkUserIDByPhonenum,
+} from './user.ts';
 import { assertEquals } from 'https://deno.land/std@0.152.0/testing/asserts.ts';
 
 const DINGTALK_ACCESSTOKEN = Deno.env.get('DINGTALK_ACCESSTOKEN') ?? '';
 const DINGTALK_USERID = Deno.env.get('DINGTALK_USERID') ?? '';
+const DINGTALK_PHONENUM = Deno.env.get('DINGTALK_PHONENUM') ?? '';
 
 Deno.test('getDingtalkUserByUserID', async () => {
   const res = await getDingtalkUserByUserID(
@@ -19,4 +24,13 @@ Deno.test('getDingtalkUserByDeptID', async () => {
     DINGTALK_ACCESSTOKEN,
   );
   console.log('res', res);
+});
+
+Deno.test('getDingtalkUserIDByPhonenum', async () => {
+  const res = await getDingtalkUserIDByPhonenum(
+    DINGTALK_PHONENUM,
+    DINGTALK_ACCESSTOKEN,
+  );
+  console.log('res', res);
+  assertEquals(typeof res?.body?.userid, 'string');
 });
