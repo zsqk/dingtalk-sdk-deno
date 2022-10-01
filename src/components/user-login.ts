1013b69b78b43c37e248a5c71b6e9db5ecf16071
iugo <iugogogo@gmail.com>
2022-10-01 10:09:30 +0800

    opt: 完善 获取钉钉用户基础信息 参数及返回值

--- src/components/user-login.ts
+++ src/components/user-login.ts
@@ -1,8 +1,8 @@
 /**
  * [Dingtalk] 获取钉钉用户基础信息 (通过免登码获取用户信息)
  * [doc](https://bit.ly/3sXTWuT)
- * @param code
- * @param token
+ * @param code 免登授权码
+ * @param token 调用服务端 API 的应用凭证
  * @returns
  *
  * @author Lian Zheren <lzr@go0356.com>
@@ -11,11 +11,24 @@ export async function getDingtalkUserBaseByCode(
   code: string,
   token: string,
 ): Promise<{
+  /** 设备ID */
   deviceID: string;
+  /** 用户名字 */
   name: string;
+  /** 用户的 unionID */
   unionID: string;
+  /** 用户的 userID */
   userID: string;
+  /** 是否是管理员 */
   isAdmin: boolean;
+  /**
+   * 级别。
+   * 1：主管理员
+   * 2：子管理员
+   * 100：老板
+   * 0：其他（如普通员工）
+   */
+  sysLevel: 1 | 2 | 100 | 0;
 }> {
   const path = 'https://oapi.dingtalk.com/topapi/v2/user/getuserinfo';
   const q = new URLSearchParams({ access_token: token });
@@ -39,12 +52,13 @@ export async function getDingtalkUserBaseByCode(
     typeof name !== 'string' ||
     typeof unionID !== 'string' ||
     typeof userID !== 'string' ||
-    typeof isAdmin !== 'boolean' ||
-    typeof sysLevel !== 'number'
+    typeof isAdmin !== 'boolean'
   ) {
     throw TypeError('dd error');
   }
+  if (sysLevel !== 1 && sysLevel !== 2 && sysLevel !== 100 && sysLevel !== 0) {
+    throw TypeError('dd error sysLevel');
+  }
   // 实测没有 associated_unionid 该属性
-  // sys_level: 1 | 2 | 100 | 0
-  return { deviceID, name, unionID, userID, isAdmin };
+  return { deviceID, name, unionID, userID, isAdmin, sysLevel };
 }
